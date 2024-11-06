804ebcf9fe5360fa79d801dae36e3b352d835d30
Tai-Ning <winnie1533@gmail.com>
2024-11-06 19:14:45 +0800

    missing l

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const knex = require('knex')
 const db = knex({
     client: 'pg',
     connection: {
-    connectionString:process.env.DATABASE_UR,    
+    connectionString:process.env.DATABASE_URL,    
     ssl:{ rejectUnauthorized: false } 
   }
 })
